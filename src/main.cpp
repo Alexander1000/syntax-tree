f7ac18e4cc157a481f95e50c4b92691b7eec3d50
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2020-12-29 14:11:43 +0300

    parse tokens

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <syntax-tree.h>
 #include <io-buffer.h>
+#include <iostream>
 
 int main(int argc, char** argv) {
     IOBuffer::IOFileReader reader(argv[1]);
     IOBuffer::CharStream charStream(&reader);
     SyntaxTree::Lexer lexer(&charStream);
-    lexer.getNextToken();
+    auto token = lexer.getNextToken();
+    while (token != nullptr) {
+        auto reader = token->getReader();
+        char* str = (char*) malloc(sizeof(char) * 1024);
+        memset(str, 0, sizeof(char) * 1024);
+        reader->read(str, 1023);
+        std::cout << "Token: " << str << std::endl;
+        reader = token->getReader();
+    }
     return 0;
 }
