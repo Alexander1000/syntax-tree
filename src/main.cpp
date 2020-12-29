83407a09ac66a65c938f4f8ac93e4bb27de532ac
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2020-12-29 14:07:17 +0300

    syntax tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <syntax-tree.h>
+#include <io-buffer.h>
 
-int main() {
+int main(int argc, char** argv) {
+    IOBuffer::IOFileReader reader(argv[1]);
+    IOBuffer::CharStream charStream(&reader);
+    SyntaxTree::Lexer lexer(&charStream);
+    lexer.getNextToken();
     return 0;
 }
