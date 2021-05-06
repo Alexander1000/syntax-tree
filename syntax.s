703c20accee59fc3c692073b515b91497c51968a
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-06 11:39:28 +0300

    update new syntax

--- syntax.s
+++ syntax.s
@@ -3,6 +3,7 @@
 # s - syntax rule
 
 rule [t:type(t) t:doubleDot t:name t:openBrace t:matchValue t:closeBrace]
+rule [t:type(s) t:doubleDot t:name t:quantifier]
 rule [t:type(s) t:doubleDot t:name]
 rule [t:type(t) t:doubleDot t:name t:quantifier]
 rule [t:type(t) t:doubleDot t:name]
