5b723d4c2b20d84ffa1dca3a86a018887bea8cad
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2021-05-07 11:46:16 +0300

    render quantifier

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -144,7 +144,21 @@ namespace SyntaxTree::Render
                     if (t->getType() == SyntaxTree::Token::Type::QuantifierType) {
                         strQuantifierRule = (char *) malloc(sizeof(char) * 1024);
                         memset(strQuantifierRule, 0, sizeof(char) * 1024);
-                        sprintf(strQuantifierRule, "auto q%02d = new SyntaxTree::Syntax::Quantity(SyntaxTree::Syntax::QuantityType::OneOrMoreMatchType);", numberOfRecord);
+                        char* quantifierType = (char*) malloc(sizeof(char) * 16);
+                        memset(quantifierType, 0, sizeof(char) * 16);
+                        t->getReader()->read(quantifierType, 1);
+                        if (quantifierType[0] == '+') {
+                            sprintf(strQuantifierRule,
+                                "auto q%02d = new SyntaxTree::Syntax::Quantity(SyntaxTree::Syntax::QuantityType::OneOrMoreMatchType);",
+                                numberOfRecord
+                            );
+                        }
+                        if (quantifierType[0] == '?') {
+                            sprintf(strQuantifierRule,
+                                "auto q%02d = new SyntaxTree::Syntax::Quantity(SyntaxTree::Syntax::QuantityType::ZeroOrOneMatchType);",
+                                numberOfRecord
+                            );
+                        }
                         sprintf(strInnerRule, R"("%s", q%02d)", strName, numberOfRecord);
                     }
                 }
